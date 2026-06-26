dc4d0a4c93deacc9a1b96936f2ed0119d0472e66
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-06-26 11:24:53 +0200

    [UP] Update chat example.

--- examples/chat/chat_client.c
+++ examples/chat/chat_client.c
@@ -1,6 +1,7 @@
 // KRONKNET - CHAT EXEMPLE (client)
 #include "kronknet/callback/callback.h"
 #include "kronknet/client/client.h"
+#include "kronknet/errdef.h"
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -15,6 +16,24 @@
 __attribute__((constructor)) static void set_stdin_nonblocking(void)
 {
     fcntl(0, F_SETFL, O_NONBLOCK);
+}
+
+static int __connectCb(
+    knClient *client __attribute__((unused))
+)
+{
+    printf("Connected to server\n");
+    return KNEVTOK;
+}
+
+static int __readCb(
+    knClient *client __attribute__((unused)),
+    const void *data,
+    size_t length
+)
+{
+    printf(">> %.*s", (int)length, (const char *)data);
+    return KNEVTOK;
 }
 
 int main(
@@ -34,6 +53,9 @@ int main(
     }
     // NOTE: Make client able to log
     knClient_setLogging(client, true);
+    // NOTE: Set client callbacks
+    knClient_onConnectionCallback(client, &__connectCb);
+    knClient_onReadCallback(client, &__readCb);
     // NOTE: Connect client to server
     if (knClient_connect(client, argv[1], strtol(argv[2], NULL, 10)) != 0) {
         printf("Unable to connect...\n");
--- examples/chat/chat_server.c
+++ examples/chat/chat_server.c
@@ -7,9 +7,29 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define CHOK   0
 #define CHERR 84
+
+#define MAXUSR    8
+#define MAXMSG 4096
+
+struct usr {
+
+    knConnection* socket;
+    char          lastmsg[MAXMSG];
+    bool          hasmsg;
+
+};
+
+struct ctx {
+
+    struct usr users[MAXUSR];
+    size_t     usrcount;
+    knServer*  server;
+
+};
 
 // NOTE: OnConnection callback
 static int __connectCb(
@@ -17,14 +37,25 @@ static int __connectCb(
     knConnection *connection
 )
 {
+    struct ctx* data = (struct ctx*)knServer_getData(server);
     printf(
         "New connection from %s:%hu\n",
         knConnection_getIp(connection),
         knConnection_getPort(connection)
     );
+    if (data->usrcount >= MAXUSR) {
+        printf("Server is full, refusing.\n");
+        return KNEVTKICK;
+    }
+    data->users[data->usrcount] = (struct usr){
+        .socket = connection,
+        .lastmsg = {0},
+        .hasmsg = false
+    };
+    knConnection_setData(connection, &data->users[data->usrcount]);
+    data->usrcount++;
     return KNEVTOK;
 }
-
 
 // NOTE: OnDisconnection callback
 static int __disconnectCb(
@@ -32,11 +63,18 @@ static int __disconnectCb(
     knConnection *connection
 )
 {
+    struct ctx* data = (struct ctx*)knServer_getData(server);
     printf(
         "Lost connection from %s:%hu\n",
         knConnection_getIp(connection),
         knConnection_getPort(connection)
     );
+    for (size_t i = 0; i < data->usrcount; ++i) {
+        if (data->users[i].socket == connection) {
+            data->users[i] = data->users[--data->usrcount];
+            break;
+        }
+    }
     return KNEVTOK;
 }
 
@@ -46,6 +84,7 @@ static int __readCb(
     size_t length
 )
 {
+    struct usr* user = (struct usr*)knConnection_getData(connection);
     printf(
         "Received \"%.*s\" from %s:%hu\n",
         (int)length,
@@ -53,6 +92,8 @@ static int __readCb(
         knConnection_getIp(connection),
         knConnection_getPort(connection)
     );
+    strncpy(user->lastmsg, (char *)data, length);
+    user->hasmsg = true;
     return KNEVTOK;
 }
 
@@ -61,6 +102,12 @@ int main(
     char *const *argv
 )
 {
+    // NOTE: Create context
+    struct ctx context = {
+        .server = NULL,
+        .users = {0},
+        .usrcount = 0,
+    };
     if (argc < 2) {
         return CHERR;
     }
@@ -69,6 +116,8 @@ int main(
     if (!server) {
         return CHERR;
     }
+    // NOTE: Affect knServer to ctx.server
+    context.server = server;
     // NOTE: Log server's IP and port
     printf(
         "Server is running on %s:%hu\n",
@@ -77,11 +126,24 @@ int main(
     );
     // NOTE: Make the server able to log
     knServer_setLogging(server, true);
+    // NOTE: Set the context as server data
+    knServer_setData(server, &context);
+    // NOTE: Set servers callback
     knServer_onConnectionCallback(server, &__connectCb);
     knServer_onDisconnectionCallback(server, &__disconnectCb);
     knServer_onReadCallback(server, &__readCb);
     // NOTE: Run the server
-    knServer_run(server);
+    while (knServer_isRunning(server)) {
+        knServer_runOnce(server, -1);
+        for (size_t i = 0; i < context.usrcount; ++i) {
+            if (!context.users[i].hasmsg) continue;
+            for (size_t j = 0; j < context.usrcount; ++j) {
+                if (context.users[j].socket == context.users[i].socket) continue;
+                knConnection_send(context.users[j].socket, context.users[i].lastmsg, strlen(context.users[i].lastmsg));
+            }
+            context.users->hasmsg = false;
+        }
+    }
     // NOTE: Destroy server
     knServer_destroy(server);
     return CHOK;
