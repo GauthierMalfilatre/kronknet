f2fe631e4fea725fe07f395ffbbe20bd421ecfbe
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-06-27 10:55:03 +0200

    [ADD] Add knMap util definition.

---
+++ src/utils/hashmap/hash.c
@@ -0,0 +1,22 @@
+/*
+** FREE PROJECT, 2026
+** KRONKNET
+** File description:
+** Thomas Wang hash algorithm: https://gist.github.com/badboy/6267743
+*/
+#include <stdint.h>
+#include "hashmap.h"
+
+uint64_t knMap_basicHash(
+    uint64_t key
+)
+{
+    key = (~key) + (key << 21); 
+    key = key ^ (key >> 24);
+    key = (key + (key << 3)) + (key << 8);
+    key = key ^ (key >> 14);
+    key = (key + (key << 2)) + (key << 4);
+    key = key ^ (key >> 28);
+    key = key + (key << 31);
+    return key;
+}
---
+++ src/utils/hashmap/hashmap.h
@@ -0,0 +1,31 @@
+/*
+** FREE PROJECT, 2026
+** KRONKNET
+** File description:
+** Header file for KRONKNET, a simple ring-buffer library in C.
+*/
+#ifndef KRONKNET_HASHMAP_IMPL_H
+    #define KRONKNET_HASHMAP_IMPL_H
+    #include "kronknet/utils/hashmap/hashmap.h"
+    #include <stddef.h>
+    #include <stdint.h>
+
+typedef struct kronknet_hashmap_node_s {
+
+    uint64_t                        key;        //!< The unhashed key (ip << 32 | port)
+    void*                           connection; //!< The connection data ptr
+    struct kronknet_hashmap_node_s* next;       //!< The next node
+    struct kronknet_hashmap_node_s* prev;       //!< The previous node
+
+} knMapNode;
+
+// NOTE: Using for UDP mode in kronknet
+typedef struct kronknet_hashmap_s {
+
+    size_t      size;  //!< The size of the hashmap
+    knMapNode** raw;   //!< The raw array of nodes
+    knMapHash   hash;  //!< The hash function
+
+} knMap;
+
+#endif /* KRONKNET_HASHMAP_IMPL_H */
